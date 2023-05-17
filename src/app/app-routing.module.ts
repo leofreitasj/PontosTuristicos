5720976d0da55920d5c126a78d267b63ade72002
leofreitasj <leonardofreitasj@gmail.com>
2023-05-17 16:15:48 -0300

    Atualização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'sobre',component:SobreComponent},
+  {path:'contato',component:ContatoComponent},
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
